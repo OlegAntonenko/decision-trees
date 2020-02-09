75528148e62a59c6b01bce6fce4c181fbc0b16bc
OlegAntonenko <oleg.antonenko.98@inbox.ru>
2020-02-09 16:26:04 +0700

    Use tree with example

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from c45 import C45
 
 
-tree = C45("iris-data.txt", "iris-names.txt")
+tree = C45("C:\\Users\\Олег\\Documents\\Диплом\\data\\iris-data.txt",
+           "C:\\Users\\Олег\\Documents\\Диплом\\data\\iris-names.txt")
 tree.extract_data()
 tree.preprocess_data()
 tree.generate_tree()
 tree.print_tree()
+
+# Example
+iris = [2.9, 1.0, 3.4, 4.2]
+classObj = tree.use_tree(iris)
+print(classObj)
