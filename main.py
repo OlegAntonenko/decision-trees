632edf9ccb5f9a50691994d5bb45126f14d8ccc5
OlegAntonenko <oleg.antonenko.98@inbox.ru>
2020-02-19 02:45:47 +0700

    Corrective main

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from c45 import C45
 
 
-tree = C45("C:\\Users\\Олег\\Documents\\Диплом\\data\\iris-data.txt",
-           "C:\\Users\\Олег\\Documents\\Диплом\\data\\iris-names.txt")
+tree = C45("C:\\Users\\Олег\\Documents\\Диплом\\data\\banana.dat")
 tree.extract_data()
 tree.preprocess_data()
 tree.generate_tree()
 tree.print_tree()
 
-# Example
-iris = [2.9, 1.0, 3.4, 4.2]
-classObj = tree.use_tree(iris)
-print(classObj)
+# iris = [2.9, 1.0, 3.4, 4.2]
+# classObj = tree.use_tree(iris)
+# print("Input: ", iris)
+# print("Output: ", classObj)
 
-# golf = ["rainy", "hot", "high", "false"]
-# classObj = tree.use_tree(golf)
-# print(golf)
-# print(classObj)
+banana = [1.14, -0.114]
+classObj = tree.use_tree(banana)
+print("Input: ", banana)
+print("Output: ", classObj)
